79c8278e68451134f353839b24db0412e602836e
Joel Tony <joeltony72@gmail.com>
2024-02-17 16:01:32 +0530

    cleanup: setup/

--- setup/compose_creator.py
+++ setup/compose_creator.py
@@ -1,3 +1,17 @@
+"""
+Used to create docker-compose file for searcher workers based on the number of workers.
+Creates a docker-compose file `docker-compose-{n}.yaml` where n is the number of workers.
+The docker-compose file will contain n+1 services:
+  1. load-balancer
+  2. searcher0
+  3. searcher1
+  ...
+  n. searcher{n-1}
+The load-balancer service will have an environment variable NUM_CHUNKS set to n.
+The searcher services will have their ports exposed from 8000 to 8000+n-1.
+The searcher services will have a volume mounted to the data directory.
+"""
+
 import os
 import sys
 
